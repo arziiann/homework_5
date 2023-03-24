e84da5ca4097d7c396b41b81592739552beb857a
arziiann <127401577+arziiann@users.noreply.github.com>
2023-03-24 15:00:51 +0400

    Add files via upload

---
+++ 7c.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+int matrix_min_element(int** array, const int rows, const int column);
+
+int main()
+{  
+	int rows = 0;
+	puts("rows = ");
+	scanf("%i", &rows);
+
+	int column = 0;
+	puts("column = ");
+	scanf("%i", &column);
+  
+	
+	int** array = (int**)calloc(rows, sizeof(int*));
+
+	for(int i = 0; i < rows; i++)
+	{
+		array[i] = (int*)calloc(column, sizeof(int));
+	}  
+	
+	printf("\n");
+       // input element	
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < column; j++)
+		{
+			 scanf("%i", &array[i][j]);
+    		}
+	
+ 	 	printf("\n");
+    	}
+       //print 2D arr
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < column; j++)
+		{
+			printf("%i ", array[i][j]);
+		}
+
+		printf("\n");
+	}
+	
+	printf("The min matrix element %i", matrix_min_element(array, rows, column));
+	
+	for(int i = 0; i < rows; i++)
+	{	
+		free(array[i]);
+	}
+	free(array);
+	return 0;
+}
+	int matrix_min_element(int** array, const int rows, const int column)
+	{
+		int min = array[0][0];
+		for(int i = 0; i < rows; i++)
+		{
+			for(int j = 0; j < column; j++)
+			{
+				if(array[i][j] < min)
+				{
+					min = array[i][j];
+				}
+			}
+		}
+	
+		return min;
+	}
+
+
